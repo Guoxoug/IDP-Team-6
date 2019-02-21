27a855e1c6b22ce09a50bb9a090cc1411edcf8ec
Greedquest <guido_spam@hotmail.com>
2019-02-21 08:34:36 +0000

    Initial Commit
    Add some Sloeber files and Eclipse stuff. Also commit tutorial files

---
+++ Arduino/Tutorials/Chicken.cpp
@@ -0,0 +1,15 @@
+/*
+ * Chicken.cpp
+ *
+ *  Created on: 19 Feb 2019
+ *      Author: Guy
+ */
+
+#include "Chicken.h"
+#include "Arduino.h"
+
+// example class that can be imported
+
+void bleep(){
+	Serial.println("cheep");
+}
