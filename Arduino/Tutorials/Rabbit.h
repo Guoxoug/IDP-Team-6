36403f752605c5a5379c9c53a019f4d4c2553a4b
Greedquest <guido_spam@hotmail.com>
2019-02-22 07:59:14 +0000

    Test class inclusion
    Just getting to grips with #including files (don't forget to serial.begin!!)

---
+++ Arduino/Tutorials/Rabbit.h
@@ -0,0 +1,18 @@
+/*
+ * Rabbit.h
+ *
+ *  Created on: 21 Feb 2019
+ *      Author: Guy
+ */
+
+#ifndef RABBIT_H_
+#define RABBIT_H_
+
+#include "Arduino.h"
+
+class Rabbit {
+public:
+	String do_something();
+};
+
+#endif /* RABBIT_H_ */
--- Arduino/Tutorials/Tutorials.ino
+++ Arduino/Tutorials/Tutorials.ino
@@ -1,22 +1,16 @@
 #include "Arduino.h"
-//#include "Chicken.h"
-
+#include "Rabbit.h"
 
 // the setup function runs once when you press reset or power the board
-int the_led = LED_BUILTIN;
+Rabbit bunny_wabbit;
 
 void setup() {
-  // Initialise digital pin LED_BUILTIN as an output.
-  pinMode(the_led, OUTPUT);
-
+	Serial.begin(9600);
 }
+
 
 // the loop function runs over and over again forever
 void loop() {
-  digitalWrite(the_led, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);                       // wait for a second
-  digitalWrite(the_led, LOW);    // turn the LED off by making the voltage LOW
-  delay(100);                       // wait for a second
-  //bleep();
-  Serial.println("oaf");
+	//Serial.print("la");
+	Serial.println(bunny_wabbit.do_something());
 }
