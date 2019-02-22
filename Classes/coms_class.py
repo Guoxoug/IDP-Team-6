0e3434c5fc31810ce9870be5310c09a4c403aab2
mattijsdepaepe@hotmail.com <33912270+mattijsdp@users.noreply.github.com>
2019-02-22 15:52:20 +0000

    Put classes in folder

---
+++ Classes/coms_class.py
@@ -0,0 +1,30 @@
+"""General Idea:
+Function           |Num     Parameter    e.g. forward 50%: b(1) b(0.5) so one byte for num then 1 for parameter
+-----------------------------------------
+forward            | 0      %power
+backward           | 1      %power
+turn_clockwise     | 2      %power
+turn_anticlockwise | 3      %power
+hall_effect request| 4      None
+infra_red request  | 5      None
+
+
+
+
+"""
+class Coms():
+    def __init__(self, position, id):
+        """Sets up a communication object"""
+        self.open = False
+
+    def send(self, message):
+        #serial something
+
+    #Below are the translation functions using scheme above
+    def turn(self, power):
+        """Sends signal to arduino to set the motor to the specified power"""
+        #translate into, pin number
+
+    def forward(self, power):
+        """Updates the position of the block"""
+        position = new_position
