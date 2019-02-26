d9a5c481e9feaa20e3a060e3a05a526e36d98f36
Greedquest <guido_spam@hotmail.com>
2019-02-26 11:01:01 +0000

    Create base class for IO and LED

--- Arduino/Devices/Devices.ino
+++ Arduino/Devices/Devices.ino
@@ -1,15 +1,42 @@
 #include "Arduino.h"
-//The setup function is called once at startup of the sketch
-int sensorPin = A0;    // select the input pin for the potentiometer
 
+class IO {
+	//Base class to define any sort of input/output by associating it with a pin
+public:
+	IO(int pin_number);
+	int pin;
+	void print_pin_state();
+	void set_power(int val);
+};
+
+IO::IO(int pin_number){ //Parameterised constructor for generic IO
+	pin = pin_number;
+	Serial.println("IO constructor");
+}
+
+void IO::print_pin_state(){
+	Serial.println(pin);
+}
+
+
+class LED: public IO { //Subclass IO
+public:
+	LED(int pin_number); //default on
+};
+
+LED::LED(int pin_number) : IO(pin_number){
+	Serial.println("LED constructor");
+	pinMode(pin, OUTPUT);
+}
+
+//IO genericIO = new IO(LED_BUILTIN);
+LED led = new LED(LED_BUILTIN);
 
 void setup() {
   Serial.begin(9600);           //Start serial and set the correct Baud Rate
+  led.print_pin_state();
 }
 
 void loop() {
-  int sensorValue = analogRead(sensorPin);
-  //Serial.println(sensorValue);
-  float voltage = map(sensorValue, 0, 1024, 0.0, 500.0)/100.0; // Divide by 1024 * 5V to get into 1-5 range
-  Serial.println(voltage);
+	//genericIO.print_pin_state();
 }
