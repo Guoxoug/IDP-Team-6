813618a51730a29b2e944fd2acdcf0f30c8a6c94
Greedquest <guido_spam@hotmail.com>
2019-02-27 10:02:36 +0000

    Fix IO subclassing
    add virtual methods to allow proper subclassing

--- Arduino/Devices/Devices.ino
+++ Arduino/Devices/Devices.ino
@@ -1,12 +1,14 @@
 #include "Arduino.h"
 
+const int max_arg = 8;
+
 class IO {
 	//Base class to define any sort of input/output by associating it with a pin
 public:
 	IO(int pin_number);
 	int pin;
 	void print_pin_state();
-	void set_power(int val);
+	virtual void set_power(int val) = 0;
 };
 
 IO::IO(int pin_number){ //Parameterised constructor for generic IO
@@ -22,6 +24,10 @@ void IO::print_pin_state(){
 class LED: public IO { //Subclass IO
 public:
 	LED(int pin_number); //default on
+	void set_power(int val){
+		int digital_state = map( val, 0, max_arg, 0, 1);
+		digitalWrite( digital_state,pin);
+	}
 };
 
 LED::LED(int pin_number) : IO(pin_number){
@@ -29,14 +35,24 @@ LED::LED(int pin_number) : IO(pin_number){
 	pinMode(pin, OUTPUT);
 }
 
+//void LED::set_power(int val){
+//	int digital_state = map( val, 0, max_arg, 0, 1);
+//	digitalWrite( digital_state,pin);
+//}
+
 //IO genericIO = new IO(LED_BUILTIN);
-LED led = new LED(LED_BUILTIN);
+LED* led = new LED(LED_BUILTIN);
 
 void setup() {
   Serial.begin(9600);           //Start serial and set the correct Baud Rate
-  led.print_pin_state();
-}
+  led->print_pin_state();
+ }
 
 void loop() {
 	//genericIO.print_pin_state();
+	//do some blinking
+	led->set_power(max_arg);
+	delay(1000)
+	led->set_power(0)
+	delay(1000)
 }
