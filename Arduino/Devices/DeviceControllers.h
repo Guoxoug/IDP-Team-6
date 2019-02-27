c264d33ee58ba53386a9845b9b522a85559aea6c
Greedquest <guido_spam@hotmail.com>
2019-02-27 20:27:53 +0000

    Add DCMotor class

--- Arduino/Devices/DeviceControllers.h
+++ Arduino/Devices/DeviceControllers.h
@@ -8,6 +8,7 @@
 #ifndef DEVICECONTROLLERS_H_
 #define DEVICECONTROLLERS_H_
 
+#include "Adafruit_MotorShield.h"
 
 class IO {
 	//Base class to define any sort of input/output by associating it with a pin
@@ -15,14 +16,23 @@ public:
 	IO(int pin_number);
 	int pin;
 	void print_pin_state();
-	virtual void set_power(int val) = 0;
+	virtual void set_power(int power) = 0;
 };
 
 
-class LED: public IO { //Subclass IO
+class LED: public IO {//subclass IO
 public:
-	LED(int pin_number); //default on
-	void set_power(int val);
+	LED(int pin_number);
+	void set_power(int power);
+};
+
+class DCMotor: public IO {
+private:
+	int direction;
+public:
+	DCMotor(int motor_port, int direction);
+	void set_power(int power);
+	Adafruit_DCMotor *motor;
 };
 
 #endif /* DEVICECONTROLLERS_H_ */
