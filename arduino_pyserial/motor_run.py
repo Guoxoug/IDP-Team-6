e76256ce797b0d47a0751a5367201a18aacbc7e6
Guoxuan Xia <guoxuan_xia_1@hotmail.com>
2019-02-27 12:47:03 +0000

    added some warning comments

--- arduino_pyserial/motor_run.py
+++ arduino_pyserial/motor_run.py
@@ -2,6 +2,9 @@ from init_connection import initialise_connection
 from Classes import coms_class
 import serial
 import time
+"""1st four lines and last 2 are basic connection requirements
+IMPORTS MAY BREAK depending on whether the code is run in an IDE or from the console
+"""
 name = initialise_connection.locate_port().device  # finds port connected to board (Arduino resets on connection)
 arduino_port = serial.Serial(name, 9600, timeout=5)  # 5 second timout for read method
 initialise_connection.handshake(arduino_port)
@@ -12,10 +15,11 @@ arduino_coms.forward(100)
 time.sleep(2)
 arduino_coms.backward(100)
 time.sleep(2)
-arduino_coms.turn(100, "clockwise")
-time.sleep(2)
-arduino_coms.turn(100, "anticlockwise")
-time.sleep(2)
+for i in range(6):
+    arduino_coms.turn(100, "clockwise")
+    time.sleep(0.3)
+    arduino_coms.turn(100, "anticlockwise")
+    time.sleep(0.3)
 arduino_coms.stop()
 
 
