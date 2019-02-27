924bbab2ffc8f9cafea3ae60c5baa2218a0a7380
Greedquest <guido_spam@hotmail.com>
2019-02-27 10:19:37 +0000

    Fix blinking
    difitalwrite(pin, value) not value,pin!!

--- Arduino/Devices/Devices.ino
+++ Arduino/Devices/Devices.ino
@@ -24,9 +24,10 @@ void IO::print_pin_state(){
 class LED: public IO { //Subclass IO
 public:
 	LED(int pin_number); //default on
-	void set_power(int val){
-		int digital_state = map( val, 0, max_arg, 0, 1);
-		digitalWrite( digital_state,pin);
+	void set_power(int val){//Expects Low or High, 0 or 1
+		//int digital_state = map( val, 0, max_arg, 0, 1);
+		digitalWrite(pin, val);
+
 	}
 };
 
@@ -35,24 +36,21 @@ LED::LED(int pin_number) : IO(pin_number){
 	pinMode(pin, OUTPUT);
 }
 
-//void LED::set_power(int val){
-//	int digital_state = map( val, 0, max_arg, 0, 1);
-//	digitalWrite( digital_state,pin);
-//}
-
 //IO genericIO = new IO(LED_BUILTIN);
-LED* led = new LED(LED_BUILTIN);
+LED* led;
 
 void setup() {
   Serial.begin(9600);           //Start serial and set the correct Baud Rate
+  led = new LED(LED_BUILTIN);
   led->print_pin_state();
- }
+
+}
 
 void loop() {
 	//genericIO.print_pin_state();
 	//do some blinking
-	led->set_power(max_arg);
-	delay(1000)
-	led->set_power(0)
-	delay(1000)
+	led->set_power(HIGH);
+	delay(1000);
+	led->set_power(LOW);
+	delay(1000);
 }
