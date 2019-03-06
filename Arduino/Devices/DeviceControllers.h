9152668aba1d1714e2cf5be08b3ac081fe18726e
Greedquest <guido_spam@hotmail.com>
2019-03-06 10:38:09 +0000

    Git fix

--- Arduino/Devices/DeviceControllers.h
+++ Arduino/Devices/DeviceControllers.h
@@ -17,32 +17,20 @@ class IO {
 public:
 	IO(int pin_number);
 	int pin;
-	void print_device_location();
+	void print_pin_state();
 	virtual void set_state(int power) = 0;
 };
 
-class Sensor: public IO {
-	//base class extends IO to make it output a value
-private:
-	int old_state = 0; //everything starts off :)
-
-public:
-	Sensor(int pin);
-	virtual int read_state() = 0;
-	bool state_changed();
-	void set_state(int power);//this function should be used to enable/disable a sensor
-};
-
-class Button : public Sensor {//could probably just be a digital sensor
-public:
-	Button(int read_pin); //doesn't really need its own constructor, since sensor sets the pinmode
-	int read_state();
-	//inherit set_state
-};
 
 class LED: public IO {//subclass IO
 public:
 	LED(int pin_number);
+	void set_state(int power);
+};
+
+class Button: public IO {//subclass IO
+public:
+	Button(int pin_number);
 	void set_state(int power);
 };
 
--- Arduino/Devices/Devices.ino
+++ Arduino/Devices/Devices.ino
@@ -1,34 +1,81 @@
 #include "Arduino.h"
+<<<<<<< master
 #include "DeviceControllers.h"
-#include "Vector.h"
+=======
 
+const int max_arg = 8;
+
+class IO {
+	//Base class to define any sort of input/output by associating it with a pin
+public:
+	IO(int pin_number);
+	int pin;
+	void print_pin_state();
+	virtual void set_power(int val) = 0;
+};
+
+IO::IO(int pin_number){ //Parameterised constructor for generic IO
+	pin = pin_number;
+	Serial.println("IO constructor");
+}
+
+void IO::print_pin_state(){
+	Serial.println(pin);
+}
+
+
+class LED: public IO { //Subclass IO
+public:
+	LED(int pin_number); //default on
+	void set_power(int val);
+};
+
+LED::LED(int pin_number) : IO(pin_number){
+	Serial.println("LED constructor");
+	pinMode(pin, OUTPUT);
+}
+>>>>>>> Clean up virtual function
+
+void LED::set_power(int val){//Expects Low or High, 0 or 1
+	//int digital_state = map( val, 0, max_arg, 0, 1);
+	digitalWrite(pin, val);
+}
 
 //IO genericIO = new IO(LED_BUILTIN);
 LED* led;
 DCMotor* right_fwd;
-Button* push_button;
-Vector<IO> outputs;
+
 
 void setup() {
   Serial.begin(9600);           //Start serial and set the correct Baud Rate
   AFMS.begin();
-
-  led = new LED(LED_BUILTIN); //these declarations must come after Serial & AFMS
-  led->print_device_location();
-//  right_fwd = new DCMotor(1,FORWARD);
-//  right_fwd->print_device_location();
-  push_button = new Button(2);
-  push_button->print_device_location();
-
+  led = new LED(LED_BUILTIN); //these declarations must come after Serial
+  led->print_pin_state();
+  right_fwd = new DCMotor(1,FORWARD);
+  right_fwd->print_pin_state();
 }
 
 void loop() {
-	//read button state
+	//genericIO.print_pin_state();
+	//do some blinking
+	led->set_state(HIGH);
+	delay(1000);
+	led->set_state(LOW);
+	delay(1000);
 
-	if (push_button->state_changed()){ //needs debouncing
-		Serial.println("boop");
-		led->set_state(push_button->read_state());
+	//do some motoring
+
+	int i;
+	Serial.println("tick");
+
+	led->set_state(HIGH);
+	for (i=0; i<255; i++) {
+		right_fwd->set_state(i);
+		delay(10);
 	}
-
-
+	led->set_state(LOW);
+	for (i=255; i!=0; i--) {
+		right_fwd->set_state(i);
+		delay(10);
+	}
 }
