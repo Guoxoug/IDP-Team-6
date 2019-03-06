daecc0ccdf70927ce355436fd0a89bb0405b6c95
Greedquest <guido_spam@hotmail.com>
2019-03-06 09:59:29 +0000

    Add a vector to hold outputs

--- Arduino/Devices/Devices.ino
+++ Arduino/Devices/Devices.ino
@@ -1,10 +1,13 @@
 #include "Arduino.h"
 #include "DeviceControllers.h"
+#include "Vector.h"
+
 
 //IO genericIO = new IO(LED_BUILTIN);
 LED* led;
 DCMotor* right_fwd;
 Button* push_button;
+Vector<IO> outputs;
 
 void setup() {
   Serial.begin(9600);           //Start serial and set the correct Baud Rate
@@ -16,12 +19,13 @@ void setup() {
 //  right_fwd->print_device_location();
   push_button = new Button(2);
   push_button->print_device_location();
+
 }
 
 void loop() {
 	//read button state
 
-	if (push_button->state_changed()){
+	if (push_button->state_changed()){ //needs debouncing
 		Serial.println("boop");
 		led->set_state(push_button->read_state());
 	}
