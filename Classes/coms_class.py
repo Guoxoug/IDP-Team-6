9a8ba338a3cc81d808566e78cefb443b2c0b2141
mattijsdepaepe@hotmail.com <33912270+mattijsdp@users.noreply.github.com>
2019-03-07 10:32:05 +0000

    Updated coms withs servo and robot_main updated

--- Classes/coms_class.py
+++ Classes/coms_class.py
@@ -1,33 +1,24 @@
 from init_connection import initialise_connection
 import numpy as np
 
-"""General Idea:
-Function           |Num     Parameter    e.g. forward 50%: b(1) b(0.5) so one byte for num then 1 for parameter
------------------------------------------
-forward            | 0      %power
-backward           | 1      %power
-turn_clockwise     | 2      %power
-turn_anticlockwise | 3      %power
-hall_effect request| 4      None
-infra_red request  | 5      None
-"""
 class Coms():
     """All messages are """
     def __init__(self, serial):
         """Sets up a communication object"""
         self.serial = serial  # Serial object
 
-    def send(self, message):
+    def send(self, message: bytearray):
         # serial something
         # message in bytes
+        if message[1] == b'\n':  # bytes value of 10 is \n in utf and ascii
+            message[1] = bytes([11])
         self.serial.write(message)
-
 
 
     def motor(self, power: int, motor_name: str):
         #  takes power and motor name (right, left)  as parameters
         #  power is between -100 and 100
-        #  power to arduino func takes 0 - 7
+        #  power to arduino func takes 0 - 255
         command = bytearray([])
         if motor_name == "right":
             if power >= 0:
@@ -36,8 +27,7 @@ class Coms():
                 command += bytes([input_power])
                 command += b"\n"
                 # newline indicates end of command to arduino
-
-                self.serial.write(command)
+                self.send(command)
 
 
             elif power < 0:
@@ -46,8 +36,7 @@ class Coms():
                 command += bytes([input_power])
                 command += b"\n"
                 # newline indicates end of command to arduino
-                self.serial.write(command)
-                #print("command", bytes(command, "utf-8"))# single character
+                self.send(command)
         if motor_name == "left":
             if power >= 0:
                 input_power = round(power / 100 * 255)
@@ -55,18 +44,30 @@ class Coms():
                 command += bytes([input_power])
                 command += b"\n"
                 # newline indicates end of command to arduino
-                self.serial.write(command)
-                #print("command", bytes(command, "utf-8"))# single character
+                self.send(command)
             elif power < 0:
                 input_power = -round(power / 100 * 255)
                 command += b"d"
                 command += bytes([input_power])
                 command += b"\n"
                 # newline indicates end of command to arduino
-                self.serial.write(command)
-                #print("command", bytes(command, "utf-8"))# single character
-    # Movement methods
-    # Self explanatory ^^
+                self.send(command)
+        if motor_name == "pulley":
+            if power >= 0:
+                input_power = round(power / 100 * 255)
+                command += b"f"
+                command += bytes([input_power])
+                command += b"\n"
+                # newline indicates end of command to arduino
+                self.send(command)
+            elif power < 0:
+                input_power = -round(power / 100 * 255)
+                command += b"g"
+                command += bytes([input_power])
+                command += b"\n"
+                # newline indicates end of command to arduino
+                self.send(command)
+
     # power always int 0-100
     def forward(self, power):
         self.motor(power, "right")
@@ -83,6 +84,42 @@ class Coms():
         else:
             self.motor(-power, "right")
             self.motor(power, "left")
+
+    def servo(self, position: int):
+        """sets position of servo in degrees 0- 180
+        NB 78 is centre, angles are measured anticlockwise
+        testing shows it can swing +-30 degrees without getting stuck"""
+
+        position = int(position)  # bytes only takes ints
+        command = bytearray([])
+        command += b"e"
+        command += bytes([position])
+        command += b"\n"
+        # newline indicates end of command to arduino
+        self.send(command)
+
+    def servo_state(self, state: str):
+        if state == "right":
+            self.servo(78 - 30)
+        elif state == "left":
+            self.servo(78 + 30)
+        elif state == "centre":
+            self.servo(78)
+
+    def pulley(self, state: str):
+        if state == "up":
+            self.motor(50, "pulley")  # placeholder power
+        elif state == "down":
+            self.motor(-50, "pulley")
+        elif state == "stop":
+            self.motor(0, "pulley")
+
+    def pulley_activate(self):
+        self.pulley("up")
+        time.sleep(2)  # placeholder time for rising pulley
+        self.pulley("down")
+        time.sleep(2)
+        self.pulley("stop")
 
     def stop(self):
         self.motor(0, "right")
--- robot_main.py
+++ robot_main.py
@@ -11,7 +11,8 @@ from Classes.camera_class import Camera
 """1st four lines and last 2 are basic connection requirements
 IMPORTS MAY BREAK depending on whether the code is run in an IDE or from the console
 """
-"""
+
+
 name = initialise_connection.locate_port().device  # finds port connected to board (Arduino resets on connection)
 arduino_port = serial.Serial(name, 9600, timeout=5)  # 5 second timout for read method
 initialise_connection.handshake(arduino_port)
@@ -19,64 +20,32 @@ initialise_connection.handshake(arduino_port)
 arduino_coms = coms_class.Coms(arduino_port)
 
 arduino_coms.stop()
-"""
-
 
 camera = Camera()
-
 blocks = camera.init_blocks()
+robot = Robot(camera, arduino_coms)
 
-#robot = Robot(camera, arduino_coms)
 robot.target = find_next_target(blocks)
-
 robot.go_towards_target()
+
 #check IR
-#if yes, check hall effect
-#
+#if yes, check hall effect update block.tested and block.nuclear
+#if safe:
+arduino_coms.servo_state("left")
+#         drop down and slide across arduino_coms something
 
-#blocks = camera.init_blocks()
-#block_target = Block(np.array([314, 32]),0)
-#robot.target = block_target
-i = 0
+#Loop again until no more block.tested = False
 
-#robot.turn(0.5, 0.1, 0.1, 40)
-#robot.move_forward(0.5, 0.1, 0.1, 1000)
-
-
-#turn speed 20, 90° for 875
-#robot.move_forward(1000) 1200 halfway across red to white line
-
-"""
-blocks = camera.init_blocks()
-
-block_target = blocks[0]
-print("Set target")
-print(block_target)
-robot.target = block_target
-
-distance, angle = robot.get_distance_angle_target()
-
-print("Starting to turn", angle)
-
-time.sleep(10)
-
-robot.turn()
 
 arduino_coms.stop()
-
-
 arduino_port.close()
-print("port closed")
-
-
-"""
+print("Port closed")
 
 time.sleep(10)
 
 if camera.open:
-    print("Closing")
     camera.close()
-
+    print("Camera closed")
 
 
 
