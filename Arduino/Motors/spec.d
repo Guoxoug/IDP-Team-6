6d0d19f5deecac54f9f4e6b3c8c528ae1f50ee97
Guoxuan Xia <guoxuan_xia_1@hotmail.com>
2019-03-15 18:54:46 +0000

    deleted test/development code, only final code remains

--- Arduino/Motors/spec.d
+++
@@ -1,2 +0,0 @@
-spec.o: \
- C:/Users/guy/Documents/Macros/Sloeber/.metadata/.plugins/org.eclipse.cdt.managedbuilder.core/spec.C
--- Arduino/Tutorials/spec.d
+++
@@ -1,2 +0,0 @@
-spec.o: \
- E:/guoxu/OneDrive/Documents/Documents/School\ \ work/IB\ Coursework/IDP/.metadata/.plugins/org.eclipse.cdt.managedbuilder.core/spec.C
